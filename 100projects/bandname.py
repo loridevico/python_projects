87f2a156b195433287f1d61501a37d654516f30a
Loredana Devico <103139722+loridevico@users.noreply.github.com>
2022-08-24 14:25:51 -0300

    Day 1 project

---
+++ 100projects/bandname.py
@@ -0,0 +1,6 @@
+name1 = input("Escreva o nome da sua cidade:\n")
+name2 = input ("Agora escreva o nome do seu animal de estimação:\n")
+
+nome_banda = name2 + " de " + name1
+
+print("O nome da sua banda pode ser:" + nome_banda)
